aa4d47cd25309f22795079a72e320da5f91e5404
Trevor Waldorf <trevor.automated@gmail.com>
2019-10-29 09:04:14 -0700

    rewrite key generator to generate arrays simply with discrete round numbers

--- designer.js
+++ designer.js
@@ -14,6 +14,7 @@ function draw() {
   splotch(250, 150, 8, 1, 255)
 }
 
+
 function splotch(xcenter, ycenter, rows, scale, color) {
   rectMode(RADIUS);
   noStroke();
@@ -21,34 +22,25 @@ function splotch(xcenter, ycenter, rows, scale, color) {
   let r = 15 * scale;
   let d = 2 * r;
   let rowcount = 0; //count index
-  let rowkey = []; //2d array of row parameters explained below
+  let rowkey;
   let yrowcenter = ycenter + d * rowcount;
-  let index = 0;
-  
-  generateKey();
 
-  function generateKey() {
-    if (rowkey.length == rows) {
-      return;
-    } else {
-      rowkey[index] = [round(random(1, 100)), round(random(-50, 50))];
-      if (uniqueRow()) {
-        index++;
-        generateKey();
-      } else {
-        generateKey();
-      }
-    }
-    function uniqueRow() {
-      for (let i = 0; i < rowkey.length; i++) {
-        return rowkey[index] == rowkey[i];
-      }
-    }
-  } 
-  //rowkey[[width coefficient, xoffset value]] where rowkey.length = number of rows (such as this.rows)
-  //this could be improved by checking the last row and ensuring that the next row is not disconnected, using a bounding box dif coef perhaps
+  rowkey = generateKey(rows); //2d array of row parameters explained below
+
   drawRows();
+  
+  function generateKey(length) {
+    let array = [];
+    for (let i = 0; i <= length; i++) {
+      array[i] = [randomRoundNumber(),randomRoundNumber()];
+    }
+    return array;
 
+    function randomRoundNumber() {
+      return round(random(1,5)) * 25;
+    }
+  }
+  
   function drawRows() {
     yrowcenter = ycenter + d * rowcount;
     fillRow();
@@ -76,6 +68,14 @@ function splotch(xcenter, ycenter, rows, scale, color) {
       } 
     }
 
+    function rowLeftAlignsWithCurrentRow(n) {
+      if (n < 0 || n > rowcount) {
+        return false;
+      } else if (getRowLeftX(n) == getRowLeftX(rowcount)) {
+        verticalGlueDown(rowcount,n);
+      }
+    }
+
     function rowEndsFurtherLeftThanCurrentRow(n) {
       if (n < 0) {
         return false;
@@ -92,18 +92,6 @@ function splotch(xcenter, ycenter, rows, scale, color) {
         return true;
       } else {
         return false;
-      }
-    }
-
-    function lastRowEclipses() {
-      if (lastRowBigger() && rowEndsFurtherLeftThanCurrentRow(rowcount - 1) && rowEndsFurtherRightThanCurrentRow(rowcount - 1)) {
-        return true;
-      }
-    }
-
-    function nextRowEclipses() {
-      if (nextRowBigger() && rowEndsFurtherLeftThanCurrentRow(rowcount + 1) && rowEndsFurtherRightThanCurrentRow(rowcount + 1)) {
-        return true;
       }
     }
 
@@ -174,23 +162,6 @@ function splotch(xcenter, ycenter, rows, scale, color) {
       )
       fill(color);
     }
-
-    // refactored
-    // if (nextRowBigger()) {
-    //   fill(0, 0, 255)
-    //   glue(
-    //     getRowRightX(rowcount),
-    //     yrowcenter + r,
-    //     getRowRightX(rowcount) + d,
-    //     yrowcenter
-    //   )
-    //   glue(
-    //     getRowLeftX(rowcount),
-    //     yrowcenter + r,
-    //     getRowLeftX(rowcount) - d,
-    //     yrowcenter
-    //   )
-    // }
     fill(color);
   }
 
@@ -224,29 +195,6 @@ function splotch(xcenter, ycenter, rows, scale, color) {
     rect(xcenter + rowkey[rowcount][1], yrowcenter, getRowWidth(rowcount), r)
     circle(getRowLeftX(rowcount), yrowcenter, d)
     circle(getRowRightX(rowcount), yrowcenter, d)
-
-    //debug points
-    // stroke(0, 150, 0);
-    // strokeWeight(10);
-    // point(getRowRightX(), yrowcenter);
-    // noStroke();
-
-    // stroke(0, 0, 200);
-    // strokeWeight(10);
-    // point(getRowLeftX(), yrowcenter);
-    // noStroke();
-  }
-
-  function getPrevRowWidth() {
-    if (rowcount != 0) {
-      return rowkey[rowcount - 1][0];
-    }
-  }
-
-  function getNextRowWidth() {
-    if (rowcount + 1 < rows) {
-      return rowkey[rowcount + 1][0];
-    }
   }
 
   function getRowLeftX(n) {
@@ -273,16 +221,11 @@ function splotch(xcenter, ycenter, rows, scale, color) {
     let b = midpoint(a[0], a[1], o[0], o[1])
 
     strokeWeight(5)
-    // stroke(120,120,10)
     point(o[0], o[1])
-    // stroke(50,220,200)
     point(e[0], e[1])
     point(c[0], c[1])
-    // stroke(120,220,110)
     point(a[0], a[1])
-    // stroke(10,20,200)
     point(b[0], b[1])
-    // noStroke()
 
     vertex(e[0], e[1])
     vertex(o[0], o[1])
