8bb634c30fcc40e12da9d0c04aa3f4ddab02a474
Trevor Waldorf <trevor.automated@gmail.com>
2019-12-24 19:29:21 -0900

    add ui support for arbitrary color picks

---
+++ domcontroller.js
@@ -0,0 +1,16 @@
+const button_add_color = document.getElementById('add-color');
+const container_color = document.getElementById('colors');
+
+button_add_color.addEventListener("mousedown", (element) => {
+    addColorField();
+});
+
+function addColorField(element) {
+    let new_li = document.createElement('li');
+    let new_input = document.createElement('input');
+    new_input.setAttribute('type','text');
+    new_input.classList.add('input-color');
+    new_li.innerHTML = '#';
+    new_li.appendChild(new_input);
+    container_color.appendChild(new_li);
+}
