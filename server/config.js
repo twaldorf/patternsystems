e6f23dd036a4f5bc8f2a65849dcc47eccd95eab3
twaldorf <trevor.automated@gmail.com>
2021-05-10 14:58:39 -0700

    utilize env vars for db connection after all

--- server/config.js
+++ server/config.js
@@ -9,10 +9,10 @@ if (process.env.NODE_ENV == 'production') {
     pgp.pg.defaults.ssl = true
     connection = {
         host: process.env.DATABASE_URL,
-        // port: process.env.DATABASE_PORT,
-        // database: process.env.DATABASE_NAME,
-        // user: process.env.DATABASE_USER,
-        // password: process.env.DATABASE_PASSWORD,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
         max: 15
     }
 } else {
