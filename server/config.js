2ddfba3ccdfa794670d2e6ad2820fab89973d896
twaldorf <trevor.automated@gmail.com>
2021-05-10 11:55:39 -0700

    update connection rules

--- server/config.js
+++ server/config.js
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV == 'production') {
     connection = {
         host: process.env.DATABASE_URL,
         // port: process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USER,
+        // database: process.env.DATABASE_NAME,
+        // user: process.env.DATABASE_USER,
         // password: process.env.DATABASE_PASSWORD,
         max: 15
     }
